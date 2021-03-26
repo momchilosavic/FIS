78610ae032249751fd62537b718a89f4ddddc51b
e5639065 <Momcilo.Savic@fisglobal.com>
2021-03-26 15:09:24 +0100

    AzureVMActivity.ps1 v0.2 added to main branch

--- Monitoring/AzureVMActivity.ps1
+++ Monitoring/AzureVMActivity.ps1
@@ -1,62 +1,69 @@
 <#
-This script retrieves information who started/stoped/restarted virtual machines.
+This script retrieves information who started/stoped/restarted virtual machines and saves information as .csv
 
 Input parameters: 
-	1) $ResourceGroupName - Name of specific resource group (DefaultValue: null)
+	1) $ResourceGroupName - Name of specific resource group (DefaultValue: null) (Mandatory)
+	2) $OutputPath - Path and name of the file data to be save to (DefaultValue: null) (Mandatory)
 	
 Output parameters:
-	Time, Event, Server name and Event initiator
+	Server Name, Time, Event initiator and Event
+	
+	** Only START event enabled. 
+	** If you want to enable other events (example: STOP) change following line: 
+		-> '$_.OperationName.LocalizedValue -match "Start" -and'
+	    -> '$_.OperationName.LocalizedValue -match "Start|Deallocate" -and'
 
 Author: Momcilo Savic
-Date: 25-Mar-2021
-Version: 0.1
+Date: 26-Mar-2021
+Version: 0.2
+#>
+
+<#
+$ResourceGroups = @("PROPHET-DEVELOPMENT", "PROPHET-DEVELOPMENT-NA2", "PROPHET-DEVELOPMENT2", "PROPHET-DEVELOPMENT2-NA2", "PROPHET-ENTERPRISE", "PROPHET-PRESALES-NA2", "PROPHET-PROFESSIONAL-NA2", "RISK-MANAGMENT-NA2", "SHERWOOD-SYSTEMS-GRP-4124.522150.9822..0000.0000.3388")
 #>
 
 param(
-	[String]$ResourceGroupName
+	[Parameter(Mandatory=$true)][String]$ResourceGroupName,
+	[Parameter(Mandatory=$true)][String]$OutputPath
 )
 
-if($ResourceGroupName.Length -eq 0){
-	Get-AzLog -ResourceGroup $ResourceGroupName | 
+$logs
+if($ResourceGroupName.Length -gt 0){
+	$logs = Get-AzLog -ResourceGroup $ResourceGroupName -MaxRecord 100000 -StartTime (Get-Date).AddDays(-90) -Status "Succeeded" | 
 		Where-Object {
-			$_.OperationName.LocalizedValue -match "Start|Stop" -and
+			$_.OperationName.LocalizedValue -match "Start" -and
 			$_.Status.LocalizedValue -match "Succeeded"
-		} |
-		Format-List @{
-				name="Time"
-				Expression = {
-					$_.EventTimeStamp
-				}
-			},
-			@{
-				name="Operation"; 
-				Expression = {
-					$_.operationname.LocalizedValue#.split("/")[2]
-				}
-			},
-			@{
-				name="ServerName"; 
-				Expression = {
-					($_.resourceid -split "/")[-1]
-				}
-			},
-			@{
-				name="Caller";
-				Expression = {
-					$_.Caller
-				}
-			}
+		}
 }
 else{
-	Get-AzLog -ResourceGroup $ResourceGroupName | 
-			Where-Object {
-				$_.OperationName.LocalizedValue -match "Start|Stop" -and
-				$_.Status.LocalizedValue -match "Succeeded"
-			} |
-			Format-List @{
+	$logs = Get-AzLog -MaxRecord 100000 -StartTime (Get-Date).AddDays(-90) -Status "Succeeded" | 
+		Where-Object {
+			$_.OperationName.LocalizedValue -match "Start" -and
+			$_.Status.LocalizedValue -match "Succeeded"
+		}
+}
+
+$res = @{}
+ForEach($log in $logs){
+	$ServerName = ($log.resourceid -split "/")[-1]
+	if($res[$ServerName] -eq $null){
+		$obj = $log |
+			Select-Object @{
+					name="ServerName"; 
+					Expression = {
+						$ServerName
+					}
+				},
+				@{
 					name="Time"
 					Expression = {
 						$_.EventTimeStamp
+					}
+				},
+				@{
+					name="Caller";
+					Expression = {
+						$_.Caller
 					}
 				},
 				@{
@@ -66,15 +73,20 @@ else{
 					}
 				},
 				@{
-					name="ServerName"; 
+					name="ResourceGroup"
 					Expression = {
-						($_.resourceid -split "/")[-1]
-					}
-				},
-				@{
-					name="Caller";
-					Expression = {
-						$_.Caller
+						$ResourceGroupName
 					}
 				}
+		$res[$ServerName] = $obj
+	}
 }
+
+$exportList = @()
+ForEach($key in $res.keys){
+	$exportList += $res[$key]# | Format-List
+}
+$exportList | Sort-Object ServerName | Export-Csv $OutputPath -NoTypeInformation
+
+$Count = $exportList.Length
+Write-Output "$ResourceGroupName resource group activity successfully exported to $OutputPath. $Count machines was active for last 90 days."
